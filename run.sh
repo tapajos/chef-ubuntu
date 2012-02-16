18bd5827b9f4d206d692d2c2b8e6c86192d28b8a
Marcos Tapajos <tapajos@gmail.com>
2012-02-15 23:15:45 -0200

    use the official package

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
+mkdir -p /etc/apt/trusted.gpg.d
+gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
+gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
 apt-get update
-apt-get -y install build-essential ruby ruby-dev rubygems libopenssl-ruby
+apt-get -y install chef
 gem install chef ohai --no-ri --no-rdoc
 chef-solo -c solo.rb -j dna.json
